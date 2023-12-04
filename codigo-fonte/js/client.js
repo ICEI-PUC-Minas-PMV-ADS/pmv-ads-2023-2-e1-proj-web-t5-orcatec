6b02ba9b589fd5d1de6643a6fc77af159a7e1e89
Joao Vitor Fochat <joaovitorfoca2001@gmail.com>
2023-12-03 21:30:39 -0300

    etapa5

---
+++ codigo-fonte/js/client.js
@@ -0,0 +1,106 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sEmpresa = document.querySelector('#m-empresa')
+const sContato = document.querySelector('#m-contato')
+const sCPF = document.querySelector('#m-cpf')
+const sCidade = document.querySelector('#m-cidade')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        if (e.target.className.indexOf('modal-container') !== -1) {
+            modal.classList.remove('active')
+        }
+    }
+
+    if (edit) {
+        sNome.value = itens[index].nome
+        sEmpresa.value = itens[index].empresa
+        sContato.value = itens[index].contato
+        sCPF.value = itens[index].cpf
+        sCidade.value = itens[index].cidade
+        id = index
+    } else {
+        sNome.value = ''
+        sEmpresa.value = ''
+        sContato.value = ''
+        sCPF.value = ''
+        sCidade.value = ''
+    }
+
+}
+
+function editItem(index) {
+
+    openModal(true, index)
+}
+
+function deleteItem(index) {
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.empresa}</td>
+    <td>${item.contato}</td>
+    <td>${item.cpf}</td>
+    <td>${item.cidade}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+    tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+
+    if (sNome.value == '' || sEmpresa.value == '' || sContato.value == '' || sCPF.value == '' || sCidade.value == '') {
+        return
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) {
+        itens[id].nome = sNome.value
+        itens[id].empresa = sEmpresa.value
+        itens[id].contato = sContato.value
+        itens[id].cpf = sCPF.value
+        itens[id].cidade = sCidade.value
+    } else {
+        itens.push({ 'nome': sNome.value, 'empresa': sEmpresa.value, 'contato': sContato.value, 'cpf': sCPF.value, 'cidade': sCidade.value })
+    }
+
+    setItensBD()
+
+    modal.classList.remove('active')
+    loadItens()
+    id = undefined
+}
+
+function loadItens() {
+    itens = getItensBD()
+    tbody.innerHTML = ''
+    itens.forEach((item, index) => {
+        insertItem(item, index)
+    })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
--- codigo-fonte/js/orcamento.js
+++ codigo-fonte/js/orcamento.js
@@ -1,84 +1,84 @@
-function createBudget() {
-  event.preventDefault();
-  // let quantity = document.getElementById("quantity-input");
-  let id;
-  let productName = document.getElementById("product-name-budget-input").value;
-  let unitPrice = document.getElementById("unit-price-budget-input").value;
-  let totalPrice = document.getElementById("total-price-budget-input").value;
-  let budgetValue = document.getElementById("amount-budget-input").value;
+// function createBudget() {
+//   event.preventDefault();
+//   let quantity = document.getElementById("quantity-input");
+//   let id;
+//   let productName = document.getElementById("product-name-budget-input").value;
+//   let unitPrice = document.getElementById("unit-price-budget-input").value;
+//   let totalPrice = document.getElementById("total-price-budget-input").value;
+//   let budgetValue = document.getElementById("amount-budget-input").value;
 
-  if (!productName || productName === "") {
-    return alert("O nome do produto deve ser informado.");
-  }
+//   if (!productName || productName === "") {
+//     return alert("O nome do produto deve ser informado.");
+//   }
 
-  if (!unitPrice || unitPrice <= 0) {
-    return alert("O valor unitário do produto deve ser informado.");
-  }
+//   if (!unitPrice || unitPrice <= 0) {
+//     return alert("O valor unitário do produto deve ser informado.");
+//   }
 
-  if (!totalPrice || totalPrice <= 0) {
-    return alert("O valor total do produto deve ser informado.");
-  }
+//   if (!totalPrice || totalPrice <= 0) {
+//     return alert("O valor total do produto deve ser informado.");
+//   }
 
-  if (!budgetValue || budgetValue <= 0) {
-    return alert("O valor total do orçamento deve ser informado.");
-  }
+//   if (!budgetValue || budgetValue <= 0) {
+//     return alert("O valor total do orçamento deve ser informado.");
+//   }
 
-  var orcamentos = JSON.parse(localStorage.getItem("orcamentos")) || [];
-  var newOrcamentos = JSON.parse(localStorage.getItem("newOrcamentos")) || 0;
+//   var orcamentos = JSON.parse(localStorage.getItem("orcamentos")) || [];
+//   var newOrcamentos = JSON.parse(localStorage.getItem("newOrcamentos")) || 0;
 
-  let dateFormatted = new Date();
-  let day = String(dateFormatted.getDate()).padStart(2, '0');
-  let month = String(dateFormatted.getMonth() + 1).padStart(2, '0');
-  let year = dateFormatted.getFullYear();
-  let novadata = `${day}/${month}/${year}`
-  const newBudget = {
-    productName: productName,
-    unitPrice: parseFloat(unitPrice),
-    totalPrice: parseFloat(totalPrice),
-    budgetValue: parseFloat(budgetValue),
-    budgetDate: novadata,
-  };
+//   let dateFormatted = new Date();
+//   let day = String(dateFormatted.getDate()).padStart(2, '0');
+//   let month = String(dateFormatted.getMonth() + 1).padStart(2, '0');
+//   let year = dateFormatted.getFullYear();
+//   let novadata = `${day}/${month}/${year}`
+//   const newBudget = {
+//     productName: productName,
+//     unitPrice: parseFloat(unitPrice),
+//     totalPrice: parseFloat(totalPrice),
+//     budgetValue: parseFloat(budgetValue),
+//     budgetDate: novadata,
+//   };
 
-  orcamentos.push(newBudget);
-  localStorage.setItem("orcamentos", JSON.stringify(orcamentos));
-  localStorage.setItem("newOrcamentos", newOrcamentos + 1);
+//   orcamentos.push(newBudget);
+//   localStorage.setItem("orcamentos", JSON.stringify(orcamentos));
+//   localStorage.setItem("newOrcamentos", newOrcamentos + 1);
 
-  alert("Orçamento criado.");
-  setTimeout(function () { }, 3000);
-  window.location.href = "dashboard.html";
-}
+//   alert("Orçamento criado.");
+//   setTimeout(function () { }, 3000);
+//   window.location.href = "dashboard.html";
+// }
 
-function renderizeAllBudgets() {
-  var orcamentos = JSON.parse(localStorage.getItem("orcamentos")) || [];
+// function renderizeAllBudgets() {
+//   var orcamentos = JSON.parse(localStorage.getItem("orcamentos")) || [];
 
-  var tabelaOrcamentos = document
-    .getElementById("tabelaOrcamentos")
-    .getElementsByTagName("tbody")[0];
+//   var tabelaOrcamentos = document
+//     .getElementById("tabelaOrcamentos")
+//     .getElementsByTagName("tbody")[0];
 
-  tabelaOrcamentos.innerHTML = "";
+//   tabelaOrcamentos.innerHTML = "";
 
-  orcamentos.forEach(function (orcamento, index) {
-    var newRow = tabelaOrcamentos.insertRow();
+//   orcamentos.forEach(function (orcamento, index) {
+//     var newRow = tabelaOrcamentos.insertRow();
 
-    var productNameCell = newRow.insertCell(0);
-    var unitPriceCell = newRow.insertCell(1);
-    var totalPriceCell = newRow.insertCell(2);
-    var budgetValueCell = newRow.insertCell(3);
-    var budgetDateCell = newRow.insertCell(4);
+//     var productNameCell = newRow.insertCell(0);
+//     var unitPriceCell = newRow.insertCell(1);
+//     var totalPriceCell = newRow.insertCell(2);
+//     var budgetValueCell = newRow.insertCell(3);
+//     var budgetDateCell = newRow.insertCell(4);
 
-    productNameCell.innerHTML = orcamento.productName;
-    unitPriceCell.innerHTML = orcamento.unitPrice.toFixed(2);
-    totalPriceCell.innerHTML = orcamento.totalPrice.toFixed(2);
-    budgetValueCell.innerHTML = orcamento.budgetValue.toFixed(2);
-    budgetDateCell.innerHTML = orcamento.budgetDate || new Date();
-  });
-}
+//     productNameCell.innerHTML = orcamento.productName;
+//     unitPriceCell.innerHTML = orcamento.unitPrice.toFixed(2);
+//     totalPriceCell.innerHTML = orcamento.totalPrice.toFixed(2);
+//     budgetValueCell.innerHTML = orcamento.budgetValue.toFixed(2);
+//     budgetDateCell.innerHTML = orcamento.budgetDate || new Date();
+//   });
+// }
 
-function getNewBudgets() {
-  var newBudgets = JSON.parse(localStorage.getItem("newOrcamentos")) || 0;
-  console.log("newB: ", newBudgets);
-  if (newBudgets) {
-    var spanElement = document.getElementById("novosOrcamentos");
-    spanElement.textContent = newBudgets;
-  }
-}
+// function getNewBudgets() {
+//   var newBudgets = JSON.parse(localStorage.getItem("newOrcamentos")) || 0;
+//   console.log("newB: ", newBudgets);
+//   if (newBudgets) {
+//     var spanElement = document.getElementById("novosOrcamentos");
+//     spanElement.textContent = newBudgets;
+//   }
+// }
